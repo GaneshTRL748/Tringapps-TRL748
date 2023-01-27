a605bd234fec32e4da59346eeb3ca0dc40026a3e
Ganeshkumar <123441945+GaneshTRL748@users.noreply.github.com>
2023-01-27 18:07:53 +0530

    Update Studentoperation.java

--- Assignments3TRL748/src/main/java/students/Studentoperation.java
+++ Assignments3TRL748/src/main/java/students/Studentoperation.java
@@ -48,6 +48,6 @@ public class Studentoperation
 		Logger l= Logger.getLogger("com.api.jar");
 	 Studentoperation a1=new Studentoperation("ganesh",8.0,'B',"20.11.2001");
 	 a1.updategpa();
-	 l.info(a1.Display());
+	 l.info("The Details:"+a1.Display());
 	}
 }
