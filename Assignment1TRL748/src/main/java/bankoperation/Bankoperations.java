dd00d9a47ead2ceea6424144ac90b0314b099805
Ganeshkumar <123441945+GaneshTRL748@users.noreply.github.com>
2023-01-27 17:23:21 +0530

    Update Bankoperations.java

--- Assignment1TRL748/src/main/java/bankoperation/Bankoperations.java
+++ Assignment1TRL748/src/main/java/bankoperation/Bankoperations.java
@@ -1,7 +1,9 @@
 package bankoperation;
 import java.util.Scanner;
+import java.util.logging.Logger;
 public class Bankoperations{
     Scanner p=new Scanner(System.in);
+    Logger l= Logger.getLogger("com.api.jar");
     String accname;
     private int pinno;
     long accno;
@@ -17,10 +19,10 @@ public class Bankoperations{
     {
         if(pinvalidation()){
               this.balance=balance+amount;
-              logger.log("Amount SuccessFully Credited");
+              l.info("Amount SuccessFully Credited");
         }
         else{
-            logger.log("Wrong Pin!!!!!!");
+            l.info("Wrong Pin!!!!!!");
         }
     }
     void withdraw(double amount)
@@ -28,20 +30,20 @@ public class Bankoperations{
         if(pinvalidation()){
         if(amount > this.balance)
         {
-            logger.log("Hi "+ this.accname +" Insufficient Balance!!!!");
+            l.info("Hi "+ this.accname +" Insufficient Balance!!!!");
         }
         else{
              this.balance=balance-amount;
-             logger.log("Amount SuccessFully Debited");
+             l.info("Amount SuccessFully Debited");
         }
         }
         else{
-            logger.log("Wrong Pin!!!");
+            l.info("Wrong Pin!!!");
         }
     }
     boolean pinvalidation()
     {
-        logger.log("Enter your PinNumber:");
+        l.info("Enter your PinNumber:");
         int pinv=p.nextInt();
         if(pinv==this.pinno)
         {
@@ -58,21 +60,21 @@ public class Bankoperations{
           return this.balance;
         }
         else{
-            logger.log("Wrong Pin!!");
+            l.info("Wrong Pin!!");
             return 0;
         }
     }
     public static void main(String[] args)
     {
-       
+    	 Logger l= Logger.getLogger("com.api.jar");
         Bankoperations a1=new Bankoperations("ganesh",1247170000153950L,200,1685);
         a1.deposit(500);
         a1.withdraw(500);
-        logger.log("Hi "+a1.accname+" Your Current balance:"+a1.balanceamount());
+        l.info("Hi "+a1.accname+" Your Current balance:"+a1.balanceamount());
         bankoperation a2=new bankoperation("Ruby",124717000153951L,5000,1247);
         a2.deposit(2000);
         a2.withdraw(10000);
-        logger.log("Hi "+a2.accname+" Your Current balance:"+a2.balanceamount());
+        l.info("Hi "+a2.accname+" Your Current balance:"+a2.balanceamount());
     }
 }
 
