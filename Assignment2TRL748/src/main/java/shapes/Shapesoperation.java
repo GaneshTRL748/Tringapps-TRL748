367b9ae1d0ff4473f32c7cc6e69f79ff26b103da
Ganeshkumar <123441945+GaneshTRL748@users.noreply.github.com>
2023-02-28 11:58:36 +0530

    Update Shapesoperation.java

--- Assignment2TRL748/src/main/java/shapes/Shapesoperation.java
+++ Assignment2TRL748/src/main/java/shapes/Shapesoperation.java
@@ -1,33 +1,36 @@
 package shapes;
-import java.util.logging.*;
+import java.io.FileDescriptor;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+import java.util.Scanner;
 public class Shapesoperation {
-	Logger l= Logger.getLogger("com.api.jar");
 	double triangle;
 	double rectangle;
 	double circle;
-        double length;
-	double width;
-	double radius;
-	double base;
-	double height;
-	double sidelength;
+    double length;
+    double width;
+    double radius;
+    double base;
+    double height;
+    double sidelength;
+    static PrintStream out=new PrintStream(new FileOutputStream(FileDescriptor.out));
     Shapesoperation(double radius)
     {
        this.radius=radius; 
-       l.info("Circle Successfully created");
+       out.println("Circle Successfully created");
     }
     Shapesoperation(double sidelength,double base,double height)
     {
         this.sidelength=sidelength;
         this.base=base;
         this.height=height;
-        l.info("Triangle Successfully created");
+       out.println("Triangle Successfully created");
     }
     Shapesoperation(double length,double width)
     {
         this.length=length;
         this.width=width;
-        l.info("Rectangle Successfully created");
+        out.println("Rectangle Successfully created");
     }
     double area(int choise)
     {
@@ -37,18 +40,17 @@ public class Shapesoperation {
         }
         else if(choise==2)
         {
-            rectangle=this.length * this.width;
-            return rectangle;
-        }
-        else if(choise==3)
-        {
+
             triangle= 0.5 * this.base * this.height;
             return triangle;
+            
         }
-        else{
-            l.info("Please Give some Choise");
-            return 0;
+        else
+        {
+        	rectangle=this.length * this.width;
+            return rectangle;
         }
+       
     }
     double perimeter(int choise)
     {
@@ -59,29 +61,72 @@ public class Shapesoperation {
         }
         else if(choise==2)
         {
-            rectangle=(this.length+this.width)*2;
+        	triangle=this.base + this.height+this.sidelength;
+            return triangle;
+            
+        }
+        else
+        {
+        	rectangle=(this.length+this.width)*2;
             return rectangle;
         }
-        else if(choise==3)
-        {
-            triangle=this.base + this.height+this.sidelength;
-            return triangle;
-        }
-        else{
-            l.info("Please Give some Choise");
-            return 0;
-        }
     }
+     public void operation(int choice)
+     {
+    	 Scanner p=new Scanner(System.in);
+    	 out.println("Enter your choice 1-area 2-perimeter");
+		  int ch=p.nextInt();
+		  if(ch==1)
+		  {
+		     out.println(area(choice));
+		  }
+		  else if(ch==2)
+		  {
+			  out.println(perimeter(choice));
+		  }
+		  else {
+			  out.println("Invalid choice!!!");
+		  }
+     }
        public static void main(String[] args) {
-    	   Logger l= Logger.getLogger("com.api.jar");
-	   Shapesoperation a1=new Shapesoperation(10);
-	   Shapesoperation a2=new Shapesoperation(8,4,5);
-	   Shapesoperation a3=new Shapesoperation(10,20);
-	   l.log(Level.INFO,()->"Area of Circle:"+a1.area(1));
-	   l.log(Level.INFO,()->"Area of Rectangle:"+a3.area(2));
-	   l.log(Level.INFO,()->"Area of Triangle:"+a2.area(3));
-	   l.log(Level.INFO,()->"PeriMeter of Circle:"+a1.perimeter(1));
-	   l.log(Level.INFO,()->"PeriMeter of Rectangle:"+a3.perimeter(2));
-	   l.log(Level.INFO,()->"Perimeter of Triangle:"+a2.perimeter(3));
+    	Scanner p=new Scanner(System.in);
+    	out.println("Enter the radius:");
+    	int radius=p.nextInt();
+	   Shapesoperation a1=new Shapesoperation(radius);
+	   out.println("Enter the length and base and height :");
+	   int length=p.nextInt();
+	   int base=p.nextInt();
+	   int height=p.nextInt();
+	   Shapesoperation a2=new Shapesoperation(length,base,height);
+	   out.println("Enter the lenght and width:");
+	   int reclength=p.nextInt();
+	   int width=p.nextInt();
+	   Shapesoperation a3=new Shapesoperation(reclength,width);
+	   while(true)
+	   {
+		   out.println("Enter your choice 1-circle 2-Tringle 3-Rectangle");
+		   int choice=p.nextInt();
+		   switch(choice) {
+		   case 1:
+		   {
+			   a1.operation(choice);
+			   break;
+		   }
+		   case 2:
+		   {
+			   a2.operation(choice);
+			   break;
+		   }
+		   case 3:
+		   {
+			   a3.operation(choice);
+			   break;
+		   }
+		   default:
+		   {
+			   System.exit(0);
+		   }
+		   }
+	   }
 	}
 }
